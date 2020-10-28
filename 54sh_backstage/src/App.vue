c4d146770f8891e05e5c8cd8c83ed0eeb145d140
wangsz12 <1002590897@qq.com>
2020-10-28 23:04:53 +0800

    构建项目

---
+++ 54sh_backstage/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <span>升华网后台</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: '',
+    data() {
+      return {
+        
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
