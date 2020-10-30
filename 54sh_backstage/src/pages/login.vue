872c55127b196fbb9a3aeb38439fbb4fcc58c9d4
wangsz12 <1002590897@qq.com>
2020-10-30 22:54:44 +0800

    构建登录页面文件

---
+++ 54sh_backstage/src/pages/login.vue
@@ -0,0 +1,28 @@
+<!--
+ * @FileDescription: 登录 页面
+ * @Author: wangsz12
+ * @Date: 10.30 22.47
+ * @LastEditors: wangz12
+ * @LastEditTime: 10.30 22.47
+ -->
+
+<template>
+  <div class="container">
+    login
+  </div>
+</template>
+
+<script>
+  export default {
+    name: '',
+    data() {
+      return {
+        
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
