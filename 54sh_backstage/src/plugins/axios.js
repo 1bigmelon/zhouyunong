51fb4cffe77bfd30f53824257858355f4de2c82e
wangsz12 <1002590897@qq.com>
2020-10-30 22:57:07 +0800

    配置axios拦截器

---
+++ 54sh_backstage/src/plugins/axios.js
@@ -0,0 +1,48 @@
+import axios from 'axios'
+
+/**
+ * TODO 配置baseURL
+ */
+export const baseURL = ''
+
+export const Axios = axios.create({
+  baseURL: baseURL,
+  timeout: 10000
+})
+
+// 配置请求拦截器
+Axios.interceptors.request.use((req) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    req.headers.Authorization = token
+  }
+
+  return req
+}, (err) => {
+  return Promise.reject(err)
+})
+
+// 配置响应拦截器
+Axios.interceptors.response.use((res) => {
+  if (res.headers.Authorization) {
+    localStorage.setItem('token', res.headers.Authorization)
+  }
+
+  if (res.data.status !== true) {
+    return Promise.reject(res)
+  }
+
+  return res
+}, (err) => {
+  if (err.response.status === 401) {
+
+    /**
+     * TODO 返回到登录界面
+     */
+
+  }
+
+  return Promise.reject(err.response.data)
+})
+
+export default Axios
