153ad63a3d8c953673983a47b7b14edd980e206c
wangsz12 <1002590897@qq.com>
2020-10-30 22:58:06 +0800

    挂载各种配置

--- 54sh_backstage/src/main.js
+++ 54sh_backstage/src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// plugins
+import Axios from './plugins/axios'
+import API from './plugins/api'
+
+// dependencies
+import Vuex from 'vuex'
+import Router from './router/router'
+import {
+  Layout
+} from 'ant-design-vue'
+
+Vue.prototype.$axios = Axios
+Vue.prototype.$api = API
+
+// antdv config
+Vue.use(Layout)
+
+Vue.use(Vuex)
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router: Router
 }).$mount('#app')
