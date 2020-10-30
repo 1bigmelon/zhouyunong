48619e10c54e1a23372332209b7bfe4657f068d0
wangsz12 <1002590897@qq.com>
2020-10-30 22:54:37 +0800

    构建主页文件

---
+++ 54sh_backstage/src/pages/index.vue
@@ -0,0 +1,35 @@
+<!--
+ * @FileDescription: 主页 页面
+ * @Author: wangsz12
+ * @Date: 10.30 22.47
+ * @LastEditors: wangz12
+ * @LastEditTime: 10.30 22.47
+ -->
+
+<template>
+  <div class="container">
+    <layout>
+      aaa
+    </layout>
+  </div>
+</template>
+
+<script>
+  import layout from '../layouts/MainLayout'
+
+  export default {
+    name: 'index',
+    components: {
+      layout
+    },
+    data() {
+      return {
+        
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
