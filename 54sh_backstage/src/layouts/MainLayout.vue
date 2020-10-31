18e0b9fa805c4a9f2e375346070faf34dba3815e
wangsz12 <1002590897@qq.com>
2020-11-01 00:17:02 +0800

    构建基本的布局

--- 54sh_backstage/src/layouts/MainLayout.vue
+++ 54sh_backstage/src/layouts/MainLayout.vue
@@ -9,20 +9,91 @@
 <template>
   <div class="container">
     <a-layout>
-      <a-layout-header style="background-color: pink;">
-        header
+      <a-layout-header class="header">
+        <span>中南大学升华网管理系统</span>
+        <span>{{ systemName }}</span>
+        <div class="user-info">
+          <a-avatar icon="user" style="margin-right: .5rem;"/>
+          <a-dropdown>
+            <span>{{ username }} <a-icon type="caret-down" style="transform: scale(0.8);"/> </span>
+            <a-menu slot="overlay">
+              <a-menu-item>
+                <a-icon type="export" />
+                <span>注销</span>
+              </a-menu-item>
+            </a-menu>
+          </a-dropdown>
+        </div>
       </a-layout-header>
-      <a-layout>
-        <a-layout-sider style="background-color: lightblue;">
-          sidebar
+      <a-layout class="main-container">
+        <a-layout-sider class="sider">
+          <a-menu class="menu" mode="inline">
+            <a-sub-menu key="article">
+              <template slot="title">
+                <a-icon type="file-text"/>
+                <span>文章管理</span>
+              </template>
+              <a-menu-item key="newArticle">
+                <a-icon type="form" />
+                <span>新建文稿</span>
+              </a-menu-item>
+              <a-menu-item key="reviewArticle">
+                <a-icon type="audit" />
+                <span>文稿审核</span>
+              </a-menu-item>
+              <a-menu-item key="articleManage">
+                <a-icon type="container" />
+                <span>文稿管理</span>
+              </a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="class">
+              <template slot="title">
+                <a-icon type="bars"/>
+                <span>分类管理</span>
+              </template>
+              <a-menu-item key="newClass">
+                <a-icon type="form" />
+                <span>新建分类</span>
+              </a-menu-item>
+              <a-menu-item key="showClass">
+                <a-icon type="appstore" />
+                <span>查看分类</span>
+              </a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="tags">
+              <template slot="title">
+                <a-icon type="tags"/>
+                <span>标签管理</span>
+              </template>
+              <a-menu-item key="newTag">
+                <a-icon type="form" />
+                <span>新建标签</span>
+              </a-menu-item>
+              <a-menu-item key="showTag">
+                <a-icon type="appstore" />
+                <span>查看标签</span>
+              </a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="system">
+              <template slot="title">
+                <a-icon type="setting"/>
+                <span>系统管理</span>
+              </template>
+              <a-menu-item key="personnel">
+                <a-icon type="team" />
+                <span>人员管理</span>
+              </a-menu-item>
+            </a-sub-menu>
+            <a-menu-item key="logout">
+              <a-icon type="export" />
+              <span>注销</span>
+            </a-menu-item>
+          </a-menu>
         </a-layout-sider>
-        <a-layout-content style="background-color: lightgreen;">
+        <a-layout-content class="content">
           content
         </a-layout-content>
       </a-layout>
-      <a-layout-footer style="background-color: lightyellow;">
-        footer
-      </a-layout-footer>
     </a-layout>
   </div>
 </template>
@@ -32,12 +103,66 @@
     name: 'MainLayout',
     data() {
       return {
-        
+        systemName: '审稿后台',
+        username: 'ABC'
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-  
+  .header {
+    background-color: rgb(20, 120, 226);
+    // background-image: linear-gradient(to right, rgb(20, 120, 226), rgb(46, 114, 187));
+    position: fixed;
+    z-index: 1;
+    width: 100%;
+
+    span {
+      color: white;
+      user-select: none;
+      -moz-user-select: none;
+      -webkit-user-select: none;
+
+      &:nth-child(1) {
+        font-size: 1.3rem;
+      }
+
+      &:nth-child(2) {
+        padding-left: 0.5rem;
+        font-size: 1rem;
+      }
+    }
+
+    .user-info {
+      width: 8.5rem;
+      padding-left: 1rem;
+      float: right;
+
+      span {
+        vertical-align: middle;
+      }
+    }
+  }
+
+  .main-container {
+    margin-top: 4rem;
+    margin-left: 200px;
+
+    .sider {
+      background-color: rgba(236, 239, 241, .3);
+      // border-right: lightgray solid 0.5px;
+      overflow: auto;
+      height: 100vh;
+      position: fixed;
+      left: 0;
+    }
+
+    .content {
+      background-color: lightgreen;
+      height: 100%;
+      width: 100%;
+      overflow: initial;
+    }
+  }
 </style>
