4f464ae2b770dcdcd548ebfa85b4f8c60b9bbd60
wangsz12 <1002590897@qq.com>
2020-11-01 23:13:09 +0800

    配置依赖与插件

--- 54sh_backstage/src/main.js
+++ 54sh_backstage/src/main.js
@@ -2,24 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 
 // plugins
-import Axios from './plugins/axios'
-import API from './plugins/api'
+import axiosConfig from './plugins/axios'
+import APIConfig from './plugins/api'
+Vue.use(axiosConfig)
+   .use(APIConfig)
 
 // dependencies
 import Vuex from 'vuex'
 import Router from './router/router'
-import {
-  Layout, Message, Menu, Icon, Avatar, Dropdown
-} from 'ant-design-vue'
-
-Vue.prototype.$axios = Axios
-Vue.prototype.$api = API
-
-// antdv config
-Vue.use(Layout).use(Message).use(Menu).use(Icon).use(Avatar)
-   .use(Dropdown)
-
 Vue.use(Vuex)
+
+// ant design vue
+import {
+  Layout, Message, Menu, Icon, Avatar, Dropdown, Button
+} from 'ant-design-vue'
+Vue.use(Layout).use(Message).use(Menu).use(Icon).use(Avatar)
+   .use(Dropdown).use(Button)
+
+// MainLayout
+import shLayout from './layouts/MainLayout'
+Vue.component('sh-layout', shLayout)
 
 Vue.config.productionTip = false
 
