4051a23a7a81c38bdf4f4c03398296efc17cad58
wangsz12 <1002590897@qq.com>
2020-11-01 23:15:39 +0800

    创建页面

---
+++ 54sh_backstage/src/pages/article/newArticle.vue
@@ -0,0 +1,34 @@
+<!--
+ * @FileDescription: 新建文稿 页面
+ * @Author: wangsz12
+ * @Date: 11.01 11.17
+ * @LastEditors: wangz12
+ * @LastEditTime: 11.01 11.16
+ -->
+
+<template>
+  <div class="container">
+    <!-- dev test -->
+      new article
+    <!-- dev test -->
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'newArticle',
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+    console.log('mounted')
+  }
+}
+</script>
+
+<style>
+
+</style>
