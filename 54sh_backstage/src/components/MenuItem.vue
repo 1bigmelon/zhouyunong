ba828fdfe77e7f42b281d79ecd737d09d57f1a74
wangsz12 <1002590897@qq.com>
2020-12-02 23:48:41 +0800

    sidebar菜单项目组件

---
+++ 54sh_backstage/src/components/MenuItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="menu-item--container">
+    <div class="menu-item--content-container">
+      <div class="menu-item--icon"></div>
+      <a-icon type="form" />
+      <span>新建文章</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+//       background-color: rgb(242, 240, 254);
+  .menu-item--container {
+    height: 2.7rem;
+    width: 100%;
+    padding-left: calc(1.8rem - 3px);
+    border-left: #FFF solid 3px;
+    display: flex;
+    align-items: center;
+    color: rgb(123, 123, 126);
+
+    &:hover {
+      background-color: rgb(247, 247, 247);
+      cursor: pointer;
+      border-left: rgb(124, 97, 241) solid 3px;
+      transition: border-left linear .2s;
+    }
+
+    .menu-item--content-container {
+      span {
+        margin-left: .8rem;
+      }
+    }
+  }
+</style>
