90d5c6220da4479214ceae89f5973c6beb017219
wangsz12 <1002590897@qq.com>
2020-12-03 23:48:18 +0800

    新建文章审核的页面

---
+++ 54sh_backstage/src/pages/article/review.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <!-- dev test -->
+      review
+    <!-- dev test -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
