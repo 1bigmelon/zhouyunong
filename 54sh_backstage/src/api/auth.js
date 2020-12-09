bf9f7bbb8b1c83991ee9618715847a56f42cc7df
wangsz12 <1002590897@qq.com>
2020-12-09 22:50:01 +0800

    添加验证token的api

--- 54sh_backstage/src/api/auth.js
+++ 54sh_backstage/src/api/auth.js
@@ -3,5 +3,8 @@ import context from '../main'
 export default {
   login(credentials) {
     return context.$axios.post('/auth/signin', credentials)
+  },
+  verifyToken() {
+    return context.$axios.get('/auth/verify')
   }
 }
