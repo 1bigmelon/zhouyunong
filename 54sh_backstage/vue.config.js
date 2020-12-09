0d36265c7e4805d1801b673335f97d65ebe6bb02
wangsz12 <1002590897@qq.com>
2020-12-09 22:49:01 +0800

    全局引入scss基类

--- 54sh_backstage/vue.config.js
+++ 54sh_backstage/vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
       sass: {
         additionalData: `
           @import '@/styles/variables.scss';
+          @import '@/styles/base.scss';
         `
       },
       // less: {
