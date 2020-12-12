23322e8b0b0444748cd50fde16918ec71c960e16
wangsz12 <1002590897@qq.com>
2020-12-12 12:04:05 +0800

    按需更新选用的antdv组件

--- 54sh_backstage/src/main.js
+++ 54sh_backstage/src/main.js
@@ -5,7 +5,7 @@ import App from './App.vue'
 import axiosConfig from './plugins/axios'
 import APIConfig from './plugins/api'
 Vue.use(axiosConfig)
-  .use(APIConfig)
+Vue.use(APIConfig)
 
 // dependencies
 import Vuex from 'vuex'
@@ -15,12 +15,14 @@ import router from './router/router'
 
 // ant design vue
 import {
-  Message, Icon, Avatar, Input, Button, Table, Tag, Tooltip
+  Message, Icon, Avatar, Input, Button,
+  Table, Tag, Tooltip, Form, Select
 } from 'ant-design-vue'
 Vue.use(Icon).use(Avatar).use(Input).use(Button).use(Table)
-  .use(Tag).use(Tooltip)
+  .use(Tag).use(Tooltip).use(Form).use(Select)
 
 Vue.prototype.$message = Message
+Vue.prototype.$form = Form
 
 Vue.config.productionTip = false
 
