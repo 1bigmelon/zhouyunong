04280ae049914d58ab40cb5ef98e478ebf6e404c
wangsz12 <1002590897@qq.com>
2020-12-19 15:53:57 +0800

    完成编辑用户的开发

--- 54sh_backstage/src/pages/user/editUser.vue
+++ 54sh_backstage/src/pages/user/editUser.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="container">
     <div class="form-box">
+      <div class="back-box" @click="goBack">
+        <a-icon type="left-circle" />
+        <span>返回</span>
+      </div>
       <a-alert :message="tipMessage" type="info" show-icon style="margin-bottom: .5rem;" />
       <a-form class="form" :form="form" :label-col="labelCol" :wrapper-col="wrapperCol" label-align="left" @submit="submitBtnHdl">
         <a-form-item label="唯一ID">
           <a-input
-            v-decorator="rules['uid']"
+            v-decorator="rules['id']"
             disabled
             allow-clear
           ></a-input>
@@ -79,8 +83,11 @@
             <a-select-option v-for="(item, index) in orgList" :key="index" :value="item.id">{{ item.name }}</a-select-option>
           </a-select>
         </a-form-item>
-        <div class="submit-box">
+        <div class="operation-box">
           <a-button type="primary" html-type="submit" :loading="submitting">提交修改</a-button>
+          <a-popconfirm title="确定重置为修改前数据？" ok-text="确定" cancel-text="取消" @confirm="reset">
+            <a-button type="danger">重置</a-button>
+          </a-popconfirm>
         </div>
       </a-form>
     </div>
@@ -89,6 +96,14 @@
 
 <script>
 /* eslint-disable camelcase */
+
+const userInfoMap = {
+  username: 'user_id',
+  realname: 'name',
+  fixedPhone: 'tel',
+  cellPhone: 'phone',
+  department: 'org'
+}
 
 export default {
   name: '',
@@ -104,8 +119,8 @@ export default {
       },
       form: this.$form.createForm(this, { name: 'new_user' }),
       rules: {
-        uid: [
-          'uid'
+        id: [
+          'id'
         ],
         username: [
           'username',
@@ -135,7 +150,7 @@ export default {
             rules: [
               {
                 min: 6,
-                message: '请输入至少6位密码!'
+                message: '请输入至少6位密码'
               },
               {
                 validator: this.validateFirstPassword,
@@ -160,7 +175,11 @@ export default {
             rules: [
               {
                 required: true,
-                message: '请输入手机号码!'
+                message: '请输入手机号码'
+              },
+              {
+                len: 11,
+                message: '请输入正确的手机号码'
               }
             ],
           },
@@ -171,7 +190,7 @@ export default {
             rules: [
               {
                 type: 'email',
-                message: '请输入正确的电子邮箱!',
+                message: '请输入正确的电子邮箱',
               }
             ],
           },
@@ -182,7 +201,7 @@ export default {
             rules: [
               {
                 required: true,
-                message: '请选择权限角色!'
+                message: '请选择权限角色'
               }
             ]
           }
@@ -193,7 +212,7 @@ export default {
             rules: [
               {
                 required: true,
-                message: '请选择所属部门!'
+                message: '请选择所属部门'
               }
             ]
           }
@@ -214,9 +233,18 @@ export default {
           return Promise.resolve()
         }
         const { id, user_id, name, tel, phone, email, role, org } = res.data.data
-        this.userInfo = { id, user_id, name, tel, phone, email, role, org }
+        this.userInfo = {
+          id,
+          username: user_id,
+          realname: name,
+          fixedPhone: tel,
+          cellPhone: phone,
+          email,
+          role,
+          department: org.name
+        }
         this.form.setFieldsValue({
-          uid: id,
+          id,
           username: user_id,
           realname: name,
           fixedPhone: tel,
@@ -243,6 +271,19 @@ export default {
       })
   },
   methods: {
+    goBack() {
+      const that = this
+      this.$modal.confirm({
+        title: '确定离开此页面？',
+        content: '在该页面上所做的更改不会保存！',
+        okText: '确定',
+        cancelText: '取消',
+        onOk() {
+          that.$router.push('/user/manage')
+        },
+        onCancel() {}
+      })
+    },
     inputBlurHdl(e) {
       const value = e.target.value
       this.validPwd = (value && value.length >= 6)
@@ -250,18 +291,52 @@ export default {
     submitBtnHdl(e) {
       e.preventDefault()
       this.form.validateFields((err, values) => {
-        /** TODO:
-         *  判断更新
-         */
-
         if (err) {
           this.$message.error(err)
           return
         }
 
-        this.submitting = true
+        const newInfo = { id: this.userInfo.id }
+        let updated = false
+        for (const key in this.userInfo) {
+          if (values[key] === undefined) continue
+          if (values[key] !== this.userInfo[key]) {
+            updated = true
+            Object.assign(newInfo, JSON.parse(`{"${userInfoMap[key]}":"${values[key]}"}`))
+          }
+        }
+        if (values.password) {
+          updated = true
+          Object.assign(newInfo, { password: values.password })
+        }
+        if (!updated) {
+          this.$modal.warning({
+            title: '编辑用户',
+            content: '貌似没有进行修改...'
+          })
+          return
+        }
 
+        this.submitting = true
+        this.$api.changeUserInfo(newInfo)
+          .then((res) => {
+            if (!res.data.status) {
+              return Promise.reject(res.data.msg)
+            }
+
+            this.$message.success('用户信息修改成功')
+            this.$router.push('/user/manage')
+          })
+          .catch((err) => {
+            this.$message.error(err?.data.msg)
+          })
+          .finally(() => {
+            this.submitting = false
+          })
       })
+    },
+    reset() {
+      this.form.setFieldsValue(this.userInfo)
     }
   }
 }
@@ -285,14 +360,36 @@ export default {
       display: flex;
       align-items: center;
       flex-direction: column;
+      position: relative;
+
+      .back-box {
+        position: absolute;
+        left: 3rem;
+        top: 2.3rem;
+        font-size: 1.1rem;
+        display: flex;
+        align-items: center;
+        transition: all .2s ease;
+
+        &:hover {
+          cursor: pointer;
+          color: $theme-color;
+          transition: all .2s ease;
+        }
+
+        span {
+          margin-left: .5rem;
+        }
+      }
 
       .form {
         width: 25rem;
         margin-top: .8rem;
 
-        .submit-box {
+        .operation-box {
           display: flex;
-          justify-content: center;
+          justify-content: space-around;
+          padding: 0 1rem;
 
           button {
             width: 30%;
