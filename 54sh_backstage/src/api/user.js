57327e0cb77f66f4cd62e2407c5c75543c4492bf
wangsz12 <1002590897@qq.com>
2020-12-25 17:41:30 +0800

    增加搜索用户的api

--- 54sh_backstage/src/api/user.js
+++ 54sh_backstage/src/api/user.js
@@ -16,5 +16,8 @@ export default {
   },
   changeUserInfo(userInfo) {
     return Axios.post('/user/chinfo', userInfo)
+  },
+  searchUsers(criteria) {
+    return Axios.post('/user/search', criteria)
   }
 }
