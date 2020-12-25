9404c3d1b545173c0365dd79360d5640adec5558
wangsz12 <1002590897@qq.com>
2020-12-25 17:42:44 +0800

    完成搜索框的设计与开发

--- 54sh_backstage/src/pages/user/manageUser.vue
+++ 54sh_backstage/src/pages/user/manageUser.vue
@@ -7,10 +7,41 @@
     </div>
     <div class="content-box">
       <div class="search-box">
-        还没想好搜索框怎么布局
+        <a-form class="form" :form="form" label-align="right" layout="inline" @submit="search">
+          <a-form-item label="真实姓名">
+            <a-input v-decorator="rules['name']" placeholder="用户真实姓名" />
+          </a-form-item>
+          <a-form-item label="权限角色">
+            <a-select v-decorator="rules['role']" placeholder="用户权限角色" style="width: 10rem;">
+              <a-select-option key="all" value="all">全部</a-select-option>
+              <a-select-option key="FirstAudit" value="一审">一审</a-select-option>
+              <a-select-option key="SecondAudit" value="二审">二审</a-select-option>
+              <a-select-option key="FinalAudit" value="终审">终审</a-select-option>
+              <a-select-option key="Admin" value="管理员">管理员</a-select-option>
+            </a-select>
+          </a-form-item>
+          <a-form-item label="所属部门">
+            <a-select v-decorator="rules['department']" placeholder="用户所属部门" style="width: 10rem;">
+              <a-select-option v-for="(item, index) in orgList" :key="index" :value="item.id">{{ item.name }}</a-select-option>
+            </a-select>
+          </a-form-item>
+          <a-form-item label="用户状态">
+            <a-select v-decorator="rules['status']" placeholder="用户状态" style="width: 10rem;">
+              <a-select-option key="all" value="all">全部</a-select-option>
+              <a-select-option key="enabled" :value="1">使用中</a-select-option>
+              <a-select-option key="disabled" :value="0">已停用</a-select-option>
+            </a-select>
+          </a-form-item>
+          <div class="operation-box">
+            <a-button type="primary" html-type="submit" :loading="searching">搜索</a-button>
+            <a-popconfirm title="确定清空？" ok-text="确定" cancel-text="取消" @confirm="reset">
+              <a style="font-size: .9rem; margin-left: 1rem;">清空</a>
+            </a-popconfirm>
+          </div>
+        </a-form>
       </div>
       <div class="user-list-box">
-        <a-table :columns="columns" row-key="id" :data-source="userList">
+        <a-table :columns="columns" row-key="id" :data-source="showedList">
           <template slot="email" slot-scope="text">
             <span>{{ text ? text : '-' }}</span>
           </template>
@@ -58,7 +89,7 @@ const columns = [
   },
   {
     title: '真实姓名',
-    key: 'realname',
+    key: 'name',
     dataIndex: 'name',
     width: '8%',
     align: 'center'
@@ -136,8 +167,24 @@ export default {
       totalUser: -1,
       enabledUser: -1,
       disabledUser: -1,
+      form: this.$form.createForm(this, { name: 'search' }),
+      rules: {
+        name: ['name'],
+        role: ['role', {
+          initialValue: 'all'
+        }],
+        department: ['department', {
+          initialValue: 'all'
+        }],
+        status: ['status', {
+          initialValue: 'all'
+        }]
+      },
+      orgList: [],
+      searching: false,
       columns,
-      userList: []
+      userList: [],
+      showedList: []
     }
   },
   mounted() {
@@ -151,14 +198,103 @@ export default {
         this.userList = users.map((item) => Object.assign(item, {
           // eslint-disable-next-line camelcase
           last_login: moment.parseZone(item.last_login.substr(5, item.last_login.length - 3)).format('YYYY[-]MM[-]DD HH[:]mm[:]ss'),
-          statusText: item.status ? '使用中' : '已停用'
+          statusText: item.status ? '使用中' : '已停用',
+          department: item.org?.id
         }))
+        this.showedList = this.userList
         this.totalUser = users.length
         this.enabledUser = enabled
         this.disabledUser = disabled
       })
+
+    this.$api.getAllOrgs()
+      .then((res) => {
+        if (!res.data.status) {
+          return Promise.reject(res.data.msg)
+        }
+        this.orgList = [
+          {
+            name: '全部',
+            id: 'all'
+          },
+          ...res.data.data.orgs
+        ]
+      })
+      .catch((err) => {
+        this.$message.error(err?.message)
+      })
   },
   methods: {
+    reset() {
+      this.form.setFieldsValue({
+        name: '',
+        role: 'all',
+        department: 'all',
+        status: 'all'
+      })
+      this.showedList = this.userList
+      this.$message.success('搜索条件已清空')
+    },
+    search(e) {
+      e.preventDefault()
+      this.form.validateFields((err, values) => {
+        if (err) {
+          this.$message.error(err)
+          return
+        }
+
+        const criteria = {}
+        for (const key in JSON.parse(JSON.stringify(values))) {
+          if (values[key] === 'all') continue
+
+          if (key === 'status') {
+            values[key] = (values[key] === 1)
+          }
+          Object.defineProperty(criteria, key, {
+            value: values[key],
+            enumerable: true
+          })
+        }
+        console.log('criteria: ', criteria)
+
+        this.showedList = this.userList.filter((item) => {
+          let flag = true
+          for (const key in criteria) {
+            console.log('item[key]: ', item[key])
+            console.log('criteria[key]: ', criteria[key])
+            if (item[key] !== criteria[key]) {
+              flag = false
+              break
+            }
+          }
+          console.log('flag: ', flag)
+          return flag
+        })
+        this.$message.success('搜索成功')
+
+        // this.searching = true
+        // this.$api.searchUsers(criteria)
+        //   .then((res) => {
+        //     console.log(res.data)
+        //     if (!res.data.status) {
+        //       return Promise.reject(res.data.msg)
+        //     }
+        //     this.userList = res.data.data.users.map((item) => Object.assign(item, {
+        //       // eslint-disable-next-line camelcase
+        //       last_login: moment.parseZone(item.last_login.substr(5, item.last_login.length - 3)).format('YYYY[-]MM[-]DD HH[:]mm[:]ss'),
+        //       statusText: item.status ? '使用中' : '已停用'
+        //     }))
+        //     this.$message.success('搜索成功')
+        //   })
+        //   .catch((err) => {
+        //     this.$message.error(err?.message)
+        //   })
+        //   .finally(() => {
+        //     this.searching = false
+        //     this.reset()
+        //   })
+      })
+    },
     edit(username) {
       this.$router.push(`/user/edit/${username}`)
     },
@@ -232,9 +368,24 @@ export default {
       width: 100%;
 
       .search-box {
-        height: 7rem;
         width: 100%;
-        border: pink solid 2px;
+        padding: .5rem 1rem;
+        padding-bottom: 1.5rem;
+
+        .form {
+          height: 100%;
+          width: 100%;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        }
+
+        .operation-box {
+
+          button {
+            width: 5.5rem;
+          }
+        }
       }
     }
   }
