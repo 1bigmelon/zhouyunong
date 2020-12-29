ae9a520ae070021cba5cdffc4c4064a507c74a75
wangsz12 <1002590897@qq.com>
2020-12-29 23:53:43 +0800

    优化数据结构

--- 54sh_backstage/src/pages/user/editUser.vue
+++ 54sh_backstage/src/pages/user/editUser.vue
@@ -6,90 +6,93 @@
         <span>返回</span>
       </div>
       <a-alert :message="tipMessage" type="info" show-icon style="margin-bottom: .5rem;" />
-      <a-form class="form" :form="form" :label-col="labelCol" :wrapper-col="wrapperCol" label-align="left" @submit="submitBtnHdl">
-        <a-form-item label="唯一ID">
-          <a-input
-            v-decorator="rules['id']"
-            disabled
-            allow-clear
-          />
-        </a-form-item>
-        <a-form-item label="用户名">
-          <a-input
-            v-decorator="rules['username']"
-            placeholder="请输入新的用户名"
-            allow-clear
-          />
-        </a-form-item>
-        <a-form-item label="真实姓名">
-          <a-input
-            v-decorator="rules['name']"
-            placeholder="请输入新的真实姓名"
-            allow-clear
-          />
-        </a-form-item>
-        <a-form-item label="密码">
-          <a-input-password
-            v-decorator="rules['firstPwd']"
-            placeholder="若不改变密码则无需填写"
-            allow-clear
-            @blur="inputBlurHdl"
-          ></a-input-password>
-        </a-form-item>
-        <a-form-item label="再次确认密码">
-          <a-input-password
-            v-decorator="rules['secondPwd']"
-            placeholder="请输入与上面一致的密码"
-            allow-clear
-          ></a-input-password>
-        </a-form-item>
-        <a-form-item label="固定电话">
-          <a-input
-            v-decorator="rules['fixedPhone']"
-            placeholder="请输入新的固定电话"
-            allow-clear
-          />
-        </a-form-item>
-        <a-form-item label="手机号码">
-          <a-input
-            v-decorator="rules['cellPhone']"
-            placeholder="请输入新的手机号码"
-            allow-clear
-          />
-        </a-form-item>
-        <a-form-item label="邮箱">
-          <a-input
-            v-decorator="rules['email']"
-            placeholder="请输入新的邮箱"
-            allow-clear
-          />
-        </a-form-item>
-        <a-form-item label="权限角色">
-          <a-select
-            v-decorator="rules['role']"
-            placeholder="请选择新的权限角色"
-          >
-            <a-select-option key="FirstAudit" value="一审">一审</a-select-option>
-            <a-select-option key="SecondAudit" value="二审">二审</a-select-option>
-            <a-select-option key="FinalAudit" value="终审">终审</a-select-option>
-            <a-select-option key="Admin" value="管理员">管理员</a-select-option>
-          </a-select>
-        </a-form-item>
-        <a-form-item label="所属部门">
-          <a-select
-            v-decorator="rules['department']"
-            placeholder="请选择新的所属部门"
-          >
-            <a-select-option v-for="(item, index) in orgList" :key="index" :value="item.id">{{ item.name }}</a-select-option>
-          </a-select>
-        </a-form-item>
-        <div class="operation-box">
-          <a-button type="primary" html-type="submit" :loading="submitting">提交修改</a-button>
-          <a-popconfirm title="确定重置为修改前数据？" ok-text="确定" cancel-text="取消" @confirm="reset">
-            <a-button type="danger">重置</a-button>
-          </a-popconfirm>
-        </div>
-      </a-form>
+      <a-spin tip="加载中..." :delay="100" size="large" :spinning="loading">
+        <a-icon slot="indicator" type="loading" spin />
+        <a-form class="form" :form="form" :label-col="labelCol" :wrapper-col="wrapperCol" label-align="left" @submit="submitBtnHdl">
+          <a-form-item label="唯一ID">
+            <a-input
+              v-decorator="rules['id']"
+              disabled
+              allow-clear
+            />
+          </a-form-item>
+          <a-form-item label="用户名">
+            <a-input
+              v-decorator="rules['username']"
+              placeholder="请输入新的用户名"
+              allow-clear
+            />
+          </a-form-item>
+          <a-form-item label="真实姓名">
+            <a-input
+              v-decorator="rules['name']"
+              placeholder="请输入新的真实姓名"
+              allow-clear
+            />
+          </a-form-item>
+          <a-form-item label="密码">
+            <a-input-password
+              v-decorator="rules['firstPwd']"
+              placeholder="若不改变密码则无需填写"
+              allow-clear
+              @blur="inputBlurHdl"
+            ></a-input-password>
+          </a-form-item>
+          <a-form-item label="再次确认密码">
+            <a-input-password
+              v-decorator="rules['secondPwd']"
+              placeholder="请输入与上面一致的密码"
+              allow-clear
+            ></a-input-password>
+          </a-form-item>
+          <a-form-item label="固定电话">
+            <a-input
+              v-decorator="rules['fixedPhone']"
+              placeholder="请输入新的固定电话"
+              allow-clear
+            />
+          </a-form-item>
+          <a-form-item label="手机号码">
+            <a-input
+              v-decorator="rules['cellPhone']"
+              placeholder="请输入新的手机号码"
+              allow-clear
+            />
+          </a-form-item>
+          <a-form-item label="邮箱">
+            <a-input
+              v-decorator="rules['email']"
+              placeholder="请输入新的邮箱"
+              allow-clear
+            />
+          </a-form-item>
+          <a-form-item label="权限角色">
+            <a-select
+              v-decorator="rules['role']"
+              placeholder="请选择新的权限角色"
+            >
+              <a-select-option key="FirstAudit" value="一审">一审</a-select-option>
+              <a-select-option key="SecondAudit" value="二审">二审</a-select-option>
+              <a-select-option key="FinalAudit" value="终审">终审</a-select-option>
+              <a-select-option key="Admin" value="管理员">管理员</a-select-option>
+            </a-select>
+          </a-form-item>
+          <a-form-item label="所属部门">
+            <a-select
+              v-decorator="rules['department']"
+              placeholder="请选择新的所属部门"
+            >
+              <a-select-option v-for="(item, index) in orgList" :key="index" :value="item.id">{{ item.name }}</a-select-option>
+            </a-select>
+          </a-form-item>
+          <div class="operation-box">
+            <a-button type="primary" html-type="submit" :loading="submitting">提交修改</a-button>
+            <a-popconfirm title="确定重置为修改前数据？" ok-text="确定" cancel-text="取消" @confirm="reset">
+              <a-button type="danger">重置</a-button>
+            </a-popconfirm>
+          </div>
+        </a-form>
+      </a-spin>
     </div>
   </div>
 </template>
@@ -109,6 +112,8 @@ export default {
   name: '',
   data() {
     return {
+      loading: true,
+      // form
       labelCol: {
         xs: { span: 24 },
         sm: { span: 8 },
@@ -180,6 +185,7 @@ export default {
       validPwd: false,
       orgList: [],
       submitting: false,
+      // tip
       tipMessage: '带红色星号的为必填，不带的为选填',
       userInfo: {}
     }
@@ -187,23 +193,14 @@ export default {
   mounted() {
     Promise.all([this.$api.getUserInfo(this.$route.params.username), this.$api.getAllOrgs()])
       .then((res) => {
-        res.forEach(({ data }) => {
-          if (!data.status) {
-            return Promise.reject(data.msg)
+        res.forEach((item) => {
+          if (!item.data.status) {
+            return Promise.reject(new Error(item.data.msg))
           }
         })
 
         const { id, user_id, name, tel, phone, email, role, org } = res[0].data.data
-        this.userInfo = {
-          id,
-          username: user_id,
-          name,
-          fixedPhone: tel,
-          cellPhone: phone,
-          email,
-          role,
-          department: org.name
-        }
+        this.userInfo = { id }
         this.form.setFieldsValue({
           id,
           username: user_id,
@@ -215,7 +212,11 @@ export default {
           department: org.name
         })
 
-        this.orgList = res.data.data.orgs
+        this.orgList = res[1].data.data.orgs
+        this.loading = false
+      })
+      .catch((err) => {
+        this.$message.error(err.message)
       })
   },
   methods: {
@@ -269,14 +270,14 @@ export default {
         this.$api.changeUserInfo(newInfo)
           .then((res) => {
             if (!res.data.status) {
-              return Promise.reject(res.data.msg)
+              return Promise.reject(new Error(res.data.msg))
             }
 
             this.$message.success('用户信息修改成功')
             this.$router.push('/user/manage')
           })
           .catch((err) => {
-            this.$message.error(err?.data.msg)
+            this.$message.error(err.message)
           })
           .finally(() => {
             this.submitting = false
