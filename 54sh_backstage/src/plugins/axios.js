8a02fdbe2aae2adcc6dd0ad3dbbf3824ba3f8504
wangsz12 <1002590897@qq.com>
2021-01-20 21:58:07 +0800

    删除调试代码

--- 54sh_backstage/src/plugins/axios.js
+++ 54sh_backstage/src/plugins/axios.js
@@ -38,7 +38,6 @@ Axios.interceptors.response.use((res) => {
 
   return res
 }, (err) => {
-  console.log('err: ', err)
   if (err.message.startsWith('timeout')) {
     err.message = '请求超时，请检查网络'
   }
