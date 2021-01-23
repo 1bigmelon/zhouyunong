5585a62ae87e9e19f186c1522b06a72b7df5b60c
wangsz12 <wangsz12@foxmail.com>
2021-01-23 20:52:09 +0800

    增加组织相关API

--- 54sh_backstage/src/api/org.js
+++ 54sh_backstage/src/api/org.js
@@ -1,7 +1,34 @@
 import { Axios } from '@/plugins/axios'
 
 export default {
+  createOrg(orgInfo) {
+    return Axios.post('/org/new', orgInfo)
+  },
   getAllOrgs() {
     return Axios.get('/org/ls')
+  },
+  getOrgsByPageNum(page) {
+    return Axios.get(`/org/ls?page=${page}`)
+  },
+  getOrgInfo(id) {
+    return Axios.post(`/org/info`, { id })
+  },
+  disableOrg(id) {
+    return Axios.post('/org/modify', {
+      id,
+      status: false
+    })
+  },
+  enableOrg(id) {
+    return Axios.post('/org/modify', {
+      id,
+      status: true
+    })
+  },
+  changeOrgInfo(orgInfo) {
+    return Axios.post('/org/modify', orgInfo)
+  },
+  searchOrg(orgInfo) {
+    return Axios.post('/org/search', orgInfo)
   }
 }
